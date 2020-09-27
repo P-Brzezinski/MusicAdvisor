84f569eaa37cfb3bb838820f47610907ee848b81
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2020-09-27 21:55:46 +0200

    Add Spotify client ID

--- src/advisor/Main.java
+++ src/advisor/Main.java
@@ -3,11 +3,39 @@ package advisor;
 import java.util.Scanner;
 
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static String clientId = "b07d74663394474199b86e460e9d01de";
+    private static String redirectURI = "http://localhost:8080";
+    private static String authorizationLink = String.format("https://accounts.spotify.com/authorize?client_id=%s&redirect_uri=%s&response_type=code", clientId, redirectURI);
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String input;
+        if (isAuthorized()) {
+            openMenu();
+        }
+    }
 
+    private static boolean isAuthorized() {
+        String input;
+        boolean isAuthorized = false;
+        while (!isAuthorized) {
+            input = scanner.nextLine();
+            switch (input) {
+                case "auth":
+                    System.out.println(authorizationLink);
+                    System.out.println("---SUCCESS---");
+                    isAuthorized = true;
+                    break;
+                default:
+                    System.out.println("Please provide access for application.");
+            }
+        }
+        return isAuthorized;
+    }
+
+    private static void openMenu() {
+        String input;
         do {
             input = scanner.nextLine();
             switch (input) {
@@ -36,6 +64,6 @@ public class Main {
                 default:
                     System.out.println("No such option");
             }
-        }while (!input.equals("exit"));
+        } while (!input.equals("exit"));
     }
 }
